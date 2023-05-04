fa933f1dc6e419d4ba8924bb736de0ff93dd3f32
alfredomvieira <48499615+alfredomvieira@users.noreply.github.com>
2023-05-04 17:55:36 -0300

    Add files via upload

---
+++ motes-code_v2.ino
@@ -0,0 +1,106 @@
+// ############# LIBRARIES ############### //
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClient.h>
+//WiFiClient wifiClient;
+
+// ############# VARIABLES ############### //
+
+const char* SSID = "Vieira"; // rede wifi
+const char* PASSWORD = "12345678"; // senha da rede wifi
+
+String BASE_URL = "http://192.168.51.141";
+
+// ############# PROTOTYPES ############### //
+
+void initSerial();
+void initWiFi();
+void httpRequest(String path);
+
+// ############### OBJECTS ################# //
+
+WiFiClient client;
+HTTPClient http;
+
+// ############## SETUP ################# //
+
+void setup() {
+  initSerial();
+  initWiFi();
+}
+
+// ############### LOOP ################# //
+
+void loop() {
+  Serial.println("[GET] /sensors - sending request...");
+  Serial.println("");
+
+  httpRequest("/");
+
+  Serial.println("");
+  delay(1000);
+
+}
+
+// ############# HTTP REQUEST ################ //
+
+void httpRequest(String path)
+{
+  String payload = makeRequest(path);
+
+  if (!payload) {
+    return;
+  }
+
+  Serial.println("##[RESULTADO]## ==> " + payload);
+
+}
+
+String makeRequest(String path)
+{
+  
+  if (http.begin(client, BASE_URL + path)){
+   int httpCode = http.GET();
+
+   if (httpCode < 0) {
+     Serial.println("request error - " + httpCode);
+     Serial.println(httpCode);
+     return "";
+
+   }
+
+   if (httpCode != HTTP_CODE_OK) {
+    return "";
+   }
+
+  String response =  http.getString();
+  http.end();
+  return response;
+  }else{ Serial.println("begin error"); return "";}
+
+  
+}
+
+// ###################################### //
+
+// implementacao dos prototypes
+
+void initSerial() {
+  Serial.begin(9600);
+}
+
+void initWiFi() {
+  delay(10);
+  Serial.println("Conectando-se em: " + String(SSID));
+
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Conectado na Rede " + String(SSID) + " | IP => ");
+  Serial.println(WiFi.localIP());
+}
