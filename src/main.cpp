2b02734485babb02d58c5d016925dfc38e0bb7fd
victor <vicfla1981@gmail.com>
2025-11-24 14:37:02 -0300

    fix: correct metrics review issues

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include <WiFiClient.h>
 #include <WiFiClientSecureBearSSL.h>
 #include <Wire.h>
-#include <stdio.h>
 
 // Layout Libraries
 #include <Icons.h>
@@ -19,76 +18,81 @@
 // Sensor Librarie
 #include "EmonLib.h"
 
+// OTA and Telnet Libraries
+#include <ArduinoOTA.h>
+#include <TelnetStream.h>
 
 // Creates an object for energy monitoring
 EnergyMonitor monitor;
 
 // ############# CONSTANTS ############### //
 
-#define PIN_SENSOR A0       // Connect This Pin to The Sensor.(A0 on NodeMCU v3)
+#define PIN_SENSOR A0       // Connect This Pin to The Sensor (A0 on NodeMCU v3)
 #define DEBUG_BUTTON_PIN 15 // Define debug button pin (D8 on NodeMCU v3)
+
 // ############# VARIABLES ###############
 
 // WiFi Network
-const char *SSID = "Morea-Mobile";      // WiFi SSID
-const char *PASSWORD = "p@ssw0rd1234**"; // WiFi Password
-// const char *SSID = "Morea-Mobile";       // SSID do WiFi
-// const char *PASSWORD = "p@ssw0rd1234**"; // Senha do WiFi
+const char *SSID = "";     // WiFi SSID
+const char *PASSWORD = ""; // WiFi Password
+
+// OTA Config
+const char* OTA_HOSTNAME = "";
+const char* OTA_PASSWORD = ""; 
 
 // URL Data
-// String url = "https://192.168.0.105";                                                                                                                  // URL do site (usando HTTP e não HTTPS)
-String url = "http://192.168.1.105";                                                                                                                    // URL do site (usando HTTP e não HTTPS)
-const uint8_t fingerprint[20] = {0x44, 0x5D, 0x07, 0x68, 0x0F, 0xBF, 0x25, 0x26, 0xE4, 0xB5, 0x04, 0x35, 0x6D, 0x91, 0xAD, 0x96, 0xFE, 0xBF, 0x40, 0x8B}; // Impressão digital do servidor
+String url = "https://morea-ifs.org/api";
+const uint8_t fingerprint[20] = {};
 
-String serializedData;
-String apiToken;\
-String path;
+// API and Device Variables
+String apiToken;
 
 // Others
-const byte interval = 5; // Sample Collection Interval (In Seconds)
+const unsigned long interval = 5000; // Sample Collection Interval (In Milliseconds)
+unsigned long previousMillis = 0;    // Variable to store the last time the code was executed
 
 // Measure Variables
-volatile byte countPulse; // Variable for the Quantity of Pulses
-float freq;               // Variable to the Frequency in Hz (Pulses per Second)
-float amperes= 0;         // Variable to store the value in amps
-float watt = 0;           // Variable to store the value in WATTS
-float kWh = 0;            // Variable to calculate the kWh for each measurement
+float amperes = 0;
+float med_amperes = 0;
+float watt = 0;
+float kWh = 0;
 unsigned long lastMeasurement = 0;
-const int tensao = 110;  // Tensão da rede elétrica em volts
+const int tensao = 127;
 int cycles = 60;
-bool DEBUG = true; // DEBUG = true (Enables the Debug Mode)
-byte i;            // Act as a Counter Variable
+bool DEBUG = true;
+byte i = 0;
+
+// Button Debounce Variables
+int lastButtonState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 // ############# PROTOTYPES ############### //
 
-void initWiFi();                 // Connect to the WiFi
-void httpRequest(String path);   // Call the Request Function and Store the Payload
-String makeRequest(String path); // Make the Request Itself
-
-void ICACHE_RAM_ATTR incpulso();
+void initWiFi();
+void handleDebugButton();
+void printLog(const String& message);
+void setupOTA();
+void setupTelnet();
 
 // ############### OBJECTS ################# //
 
-WiFiClient client;
+BearSSL::WiFiClientSecure client;
 HTTPClient http;
 JsonDocument doc;
 Layout layout(128, 64, -1);
 Icons icons;
 
+
 // ############### SETUP ################# //
 
 void setup() {
   Serial.begin(115200);
-  monitor.current(PIN_SENSOR, 60);  
+  monitor.current(PIN_SENSOR, 111.11);
+  client.setInsecure();
 
+  pinMode(DEBUG_BUTTON_PIN, INPUT_PULLUP);
 
-  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
-  client->setFingerprint(fingerprint);
-
-  pinMode(PIN_SENSOR, INPUT);                    // Configure pin Sensor as Input
-  pinMode(DEBUG_BUTTON_PIN, INPUT);              // Configure button sensor as input
-  
-  
   // Drawing Basic Layout on Display
   layout.beginLayout();
   layout.beginTimer();
@@ -100,16 +104,16 @@ void setup() {
 
   // Get Mac Address
   String macAddress = WiFi.macAddress();
-  Serial.println("Endereço Mac: " + macAddress);
+  printLog("Endereço Mac: " + macAddress);
 
-  // Init Wifi And Connect
   initWiFi();
+  setupOTA();
+  setupTelnet();
 
-  path = url + "/api/authenticate";
+  String path = url + "/authenticate";
 
-  //  Sending Mac Address to the Server
-  if (http.begin(*client, path)) {
-    // Data serializing
+  if (http.begin(client, path)) {
+    String serializedData;
     doc["macAddress"] = macAddress;
     doc["deviceIp"] = WiFi.localIP().toString();
     serializeJson(doc, serializedData);
@@ -118,219 +122,202 @@ void setup() {
     int httpResponseCode = http.POST(serializedData);
     String payload = http.getString();
 
-    if (httpResponseCode < 0) {
-      Serial.println("request error - " + httpResponseCode);
-    }
+    if (httpResponseCode > 0) {
+      DeserializationError error = deserializeJson(doc, payload);
+      if (error) {
+        printLog("Deserialization error");
+        layout.drawIcon(6, icons.failedIcon());
+      } else {
+        String deviceName = doc["deviceName"].as<String>();
+        apiToken = doc["api_token"].as<String>();
 
-    if (httpResponseCode != HTTP_CODE_OK && httpResponseCode != HTTP_CODE_CREATED) {
-      Serial.println("Falha no envio");
-      Serial.println(httpResponseCode);
-    }
+        printLog("Device Name: " + deviceName);
+        printLog("API Token: " + apiToken);
 
-    DeserializationError error = deserializeJson(doc, payload);
-
-    if (error) {
-      Serial.println("Deserialization error");
-      layout.drawIcon(6, icons.failedIcon());
-
-      return;
-    }
-
-    String deviceName = doc["deviceName"].as<String>();
-    apiToken = doc["api_token"].as<String>();
-
-    Serial.println("Device Name: " + deviceName);
-    Serial.println("API Token: " + apiToken);
-
-    if (deviceName == "null") {
-      layout.writeLine(3, "Device: Unnamed");
+        if (deviceName == "null") {
+          layout.writeLine(3, "Device: Unnamed");
+        } else {
+          layout.writeLine(3, "Device: " + deviceName);
+        }
+        layout.drawIcon(6, icons.keyIcon());
+      }
     } else {
-      layout.writeLine(3, "Device: " + deviceName);
+      printLog("Authentication request error: " + String(httpResponseCode));
+      layout.drawIcon(6, icons.failedIcon());
     }
-    layout.drawIcon(6, icons.keyIcon());
 
     doc.clear();
     http.end();
+  } else {
+      printLog("Unable to connect for authentication");
+      layout.drawIcon(6, icons.failedIcon());
   }
 }
 
 // ############### LOOP ################# //
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) // Verify if WiFi is Connected
-  {
+  ArduinoOTA.handle(); // Must be at the beginning of the loop
+
+  if (WiFi.status() != WL_CONNECTED) {
     layout.drawIcon(7, icons.loadingIcon());
-
-    initWiFi(); // Try to Reconnect if Not
+    initWiFi();
   }
 
-  if (digitalRead(DEBUG_BUTTON_PIN) == HIGH && DEBUG == false) {
-    DEBUG = true;
-    cycles = 3;
-    i = 0;
+  handleDebugButton();
 
-    layout.drawIcon(1, icons.wrenchIcon());
+  if (millis() - previousMillis >= interval) {
+    previousMillis = millis(); // Update the timer
 
-    Serial.println("Debug mode ativado.");
-  } else if (digitalRead(DEBUG_BUTTON_PIN) == HIGH && DEBUG == true) {
-    DEBUG = false;
-    cycles = 60;
-    i = 0;
+    // ############### SENSOR CODE ################# //
 
-    layout.eraseIcon(1);
+    amperes = monitor.calcIrms(1480);
+    med_amperes +=  amperes;
+    watt = amperes * tensao;
 
-    Serial.println("Debug mode desativado.");
-  }
+    unsigned long currentTime = millis();
+    if (lastMeasurement > 0) {
+      float tempoDecorrido = (currentTime - lastMeasurement) / 3600000.0;
+      kWh += (watt * tempoDecorrido) / 1000.0;
+    }
+    lastMeasurement = currentTime;
 
-  // ############### SENSOR CODE ################# //
-    countPulse = 0; // Zero the Variable
+    i++;
 
-  sei();                  // Enable Interruption
-  delay(interval * 1000); // Await the Interval X (in Millisecond)
-  // cli();               // Disable Interruption
+    layout.writeLine(0, "Corrente: " + String(amperes) + "A");
+    layout.writeLine(1, "Potencia: " + String(watt) + "w");
+    layout.writeLine(2, "Consumo: " + String(kWh, 4) + "kWh");
 
-  freq = (float)countPulse / (float)interval; // Calculate the Frequency in Pulses per Second
-
-  
-  
- // Calculate the current using the `calcIrms` function
-  amperes = monitor.calcIrms(1480);  
-  // Calculate power by multiplying current by voltage
-  watt = amperes * tensao;  
-
-  // Update the energy spent
-  unsigned long currentTime  = millis();  
-  if (lastMeasurement > 0) {
-    // Calculate elapsed time in hours
-    float tempoDecorrido = (currentTime  - lastMeasurement) / 3600000.0;  
-    // Calculates energy spent in kWh
-    kWh += watt * tempoDecorrido / 1000.0;  
-  }
-  // Update the time of the last measurement
-  lastMeasurement = currentTime ;
-  
-  i++;
-
-  layout.writeLine(0, "Corrente: " + String(amperes) + "A");
-  layout.writeLine(1, "Potencia: " + String(watt) + "w");
-  layout.writeLine(2, "Consumo: " + String(kWh) + "kWh");
-
-
-  if (DEBUG)  // Show the Data in the Monitor Serial
-  {
-    Serial.println("Corrente: " + String(amperes));
-    Serial.println("Potência: " + String(watt));
-    Serial.println("Energia gasta: " + String(kWh) + "kWh");
-    Serial.println("Número de Coletas Atuais: " + String(i));
-  }
-
-  String macAddress = WiFi.macAddress();
-  path = url + "/api/store-data";
-
-  std::unique_ptr<BearSSL::WiFiClientSecure>
-      client(new BearSSL::WiFiClientSecure);
-  client->setFingerprint(fingerprint);
-
-  //  Sending Ip Address to the Server
-  if (http.begin(*client, path) && i == cycles) {
-    // Data serializing
-    doc["apiToken"] = apiToken;
-    doc["macAddress"] = macAddress;
-    doc["measure"][0]["type"] = 2;
-    doc["measure"][0]["value"] = kWh;
-    doc["measure"][1]["type"] = 3;
-    doc["measure"][1]["value"] = watt;
-    doc["measure"][2]["type"] = 4;
-    doc["measure"][2]["value"] = amperes;
-
-    serializeJson(doc, serializedData);
-
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST(serializedData);
-    String payload = http.getString();
-
-    if (httpResponseCode < 0) {
-      Serial.println("request error - " + String(httpResponseCode));
-
-      layout.drawIcon(5, icons.failedIcon());
-
-      i = 0;
-      kWh = 0;
-      watt = 0;
-      amperes = 0;
-      
-
-      layout.updateTimer(i, interval, cycles);
+    if (DEBUG) {
+      printLog("--- Leitura ---");
+      printLog("Corrente: " + String(amperes) + " A");
+      printLog("Potência: " + String(watt) + " W");
+      printLog("Energia gasta (acumulada): " + String(kWh, 6) + " kWh");
+      printLog("Número de Coletas Atuais: " + String(i) + "/" + String(cycles));
     }
 
-    if (httpResponseCode != HTTP_CODE_OK) {
-      Serial.println("Falha no envio");
-      Serial.println(httpResponseCode);
+    if (i >= cycles) {
+      med_amperes = (med_amperes/cycles); 
+      String path = url + "/store-data";
 
-      layout.drawIcon(5, icons.failedIcon());
+      if (http.begin(client, path)) {
+        String serializedData;
+        doc["apiToken"] = apiToken;
+        doc["macAddress"] = WiFi.macAddress();
+        doc["measure"][0]["type"] = 2;
+        doc["measure"][0]["value"] = kWh;
+        doc["measure"][1]["type"] = 3;
+        doc["measure"][1]["value"] = watt;
+        doc["measure"][2]["type"] = 4;
+        doc["measure"][2]["value"] = med_amperes;
 
+        serializeJson(doc, serializedData);
+
+        http.addHeader("Content-Type", "application/json");
+        int httpResponseCode = http.POST(serializedData);
+        String payload = http.getString();
+
+        if (httpResponseCode == HTTP_CODE_OK || httpResponseCode == HTTP_CODE_CREATED) {
+          DeserializationError error = deserializeJson(doc, payload);
+          if (error) {
+            printLog("Deserialization error on response");
+            layout.drawIcon(5, icons.failedIcon());
+          } else {
+            String responseMessage = doc["message"].as<String>();
+            printLog("Response Message:  " + responseMessage);
+            layout.drawIcon(5, icons.successIcon());
+          }
+        } else {
+          printLog("Falha no envio de dados. HTTP Code: " + String(httpResponseCode));
+          printLog("Payload: " + payload);
+          layout.drawIcon(5, icons.failedIcon());
+        }
+
+        doc.clear();
+        http.end();
+      } else {
+        printLog("Falha ao iniciar conexão HTTP para envio de dados.");
+        layout.drawIcon(5, icons.failedIcon());
+      }
       i = 0;
       kWh = 0;
-      watt = 0;
-      amperes = 0;
-
-      layout.updateTimer(i, interval, cycles);
+      med_amperes = 0;
     }
-
-    DeserializationError error = deserializeJson(doc, payload);
-
-    if (error) {
-      Serial.println("Deserialization error");
-
-      layout.drawIcon(5, icons.failedIcon());
-
-      i = 0;
-      kWh = 0;
-      watt = 0;
-      amperes = 0;
-
-      layout.updateTimer(i, interval, cycles);
-
-      doc.clear();
-      http.end();
-
-      return;
-    }
-
-    String responseMessage = doc["message"].as<String>();
-    Serial.println("Response Message:  " + responseMessage);
-
-    i = 0;
-    kWh = 0;
-    watt = 0;
-    amperes = 0;
-
-    layout.updateTimer(i, interval, cycles);
-    layout.drawIcon(5, icons.successIcon());
-
-    doc.clear();
-    http.end();
+    layout.updateTimer(i, interval/1000, cycles);
   }
-  layout.updateTimer(i, interval, cycles);
 }
 
-void ICACHE_RAM_ATTR incpulso() {
-  countPulse++; // Increments Pulse Variable
-}
+// ############# FUNCTIONS ############### //
 
+void printLog(const String& message) {
+  Serial.println(message);
+  TelnetStream.println(message);
+}
 
 void initWiFi() {
   delay(10);
-  Serial.println("Connecting to: " + String(SSID));
+  printLog("Connecting to: " + String(SSID));
 
   WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
-    Serial.print(".");
+    Serial.print("."); // Using Serial here to show progress on one line
   }
-  Serial.println();
-  Serial.print("Connected to Netowk: " + String(SSID) + " | IP => ");
-  Serial.println(WiFi.localIP());
+  Serial.println(); // New line after connecting
+  printLog("Connected to Network: " + String(SSID) + " | IP => " + WiFi.localIP().toString());
 
   layout.drawIcon(7, icons.wifiIcon());
+}
+
+void setupOTA() {
+  ArduinoOTA.setHostname(OTA_HOSTNAME);
+  ArduinoOTA.setPassword(OTA_PASSWORD);
+
+  ArduinoOTA.onStart([]() {
+    printLog("Iniciando OTA...");
+  });
+  ArduinoOTA.onEnd([]() {
+    printLog("\nOTA Finalizado.");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    char buffer[64];
+    TelnetStream.println(buffer); // Progress only to Telnet to avoid flooding Serial
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    printLog("Erro OTA: " + String(error));
+  });
+
+  ArduinoOTA.begin();
+  printLog("OTA Pronto.");
+}
+
+void setupTelnet() {
+  TelnetStream.begin();
+  printLog("Telnet pronto.");
+}
+
+void handleDebugButton() {
+  int reading = digitalRead(DEBUG_BUTTON_PIN);
+
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading == LOW && lastButtonState == HIGH) {
+      DEBUG = !DEBUG;
+      if (DEBUG) {
+        cycles = 3;
+        layout.drawIcon(1, icons.wrenchIcon());
+        printLog("Debug mode ativado. Enviando dados a cada 3 coletas.");
+      } else {
+        cycles = 60;
+        layout.eraseIcon(1);
+        printLog("Debug mode desativado. Enviando dados a cada 60 coletas.");
+      }
+      i = 0;
+    }
+  }
+
+  lastButtonState = reading;
 }
